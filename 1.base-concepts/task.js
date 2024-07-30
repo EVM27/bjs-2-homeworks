df182c6f750c54474fe1a4dbf8c873d385076d0f
EVM27 <145212059+EVM27@users.noreply.github.com>
2024-07-30 22:33:57 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,39 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+      return [];
+  }
   
-  return arr;
+  if (discriminant === 0) {
+      let root = -b / (2 * a);
+      return [root];
+  }
+  
+  if (discriminant > 0) {
+      let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+      let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      return [root1, root2];
+  }
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+    
+    let loanBody = amount - contribution;
+    
+    if (loanBody <= 0) {
+        return 0;
+    }
+    
+    let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+    
+    let totalAmount = monthlyPayment * countMonths;
+    
+    totalAmount = Number(totalAmount.toFixed(2));
+    
+    return totalAmount;
 }
